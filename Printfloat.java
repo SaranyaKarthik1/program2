a3e42f441e611ee7fbb8be375c3b74d6bf095572
SaranyaKarthik1 <111961870+SaranyaKarthik1@users.noreply.github.com>
2022-08-24 22:55:03 +0530

    Add files via upload

---
+++ Printfloat.java
@@ -0,0 +1,7 @@
+public class Printfloat{  
+    public static void main(String[] args) {  
+  
+        System.out.println("The number without using string " + Math.ceil(7.50));  
+        System.out.println("The number without using string " + Math.round(7.50));
+    }  
+}  
