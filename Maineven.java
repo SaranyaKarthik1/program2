1d97ccd4ee2fcb6904907e1332197dba353938eb
SaranyaKarthik1 <111961870+SaranyaKarthik1@users.noreply.github.com>
2022-08-24 19:15:47 +0530

    Add files via upload

---
+++ Maineven.java
@@ -0,0 +1,9 @@
+public class Maineven {
+    public static void main(String args[]) {
+        int num=21;
+        if(num%2==0)
+        System.out.println(num+ " It is an even number");
+        else
+        System.out.println(num+ " It is an odd number");
+    }
+}
