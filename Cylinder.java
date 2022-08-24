105b52f0e5b51761c9b862701fe230f60387cbdc
SaranyaKarthik1 <111961870+SaranyaKarthik1@users.noreply.github.com>
2022-08-24 22:25:26 +0530

    Add files via upload

---
+++ Cylinder.java
@@ -0,0 +1,22 @@
+public class Cylinder {
+    public static void main(String args[]) {
+       
+       float r,h,sa;
+      r = 5;
+      h = 5;
+      sa = (22*r*(r+h))/7;
+      System.out.println("Surface Area of Cylinder is: "+sa);
+    
+      
+}
+        
+                //System.out.println("\n");
+                
+            
+            //System.out.print("*");
+           
+        
+        
+        //System.out.println("The area of the square is "+ A);// the area of the square formula is A=square of a;
+    
+}
