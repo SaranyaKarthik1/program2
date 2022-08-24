57bb604a9ca04674be0721cba1a6f9581f39f9d2
SaranyaKarthik1 <111961870+SaranyaKarthik1@users.noreply.github.com>
2022-08-24 22:30:08 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,14 @@
+
+    
+
+      public class Ascii{  
+public static void main(String[] args)   
+{  
+char ch1 = 'a';  
+char ch2 = 'b';  
+int asciiv1 = ch1;  
+int asciiv2 = ch2;  
+System.out.println("The ASCII value of " + ch1 + " is: " + asciiv1);  
+System.out.println("The ASCII value of " + ch2 + " is: " + asciiv2);  
+}  
+}  
