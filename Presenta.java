5a2da47061ddddceddcd7551de8cdbcd1fef6bf5
SaranyaKarthik1 <111961870+SaranyaKarthik1@users.noreply.github.com>
2022-08-24 23:08:42 +0530

    Add files via upload

---
+++ Presenta.java
@@ -0,0 +1,8 @@
+public class Presenta {  
+    public static void main(String[] args) {  
+    int [] a = new int [] {10, 20, 30, 40, 50};  
+      
+        System.out.println("Number of elements present in an  array: " + a.length);  
+    }  
+}  
+//The number of elements present in an array
