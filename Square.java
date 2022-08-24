4aeb2f0e93141c2e29c434ab4128533f5bbfa981
SaranyaKarthik1 <111961870+SaranyaKarthik1@users.noreply.github.com>
2022-08-24 19:23:59 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,7 @@
+public class Square {
+    public static void main(String args[]) {
+        int s=4;
+        int A=s*s;
+        System.out.println("The area of the square is "+ A);// the area of the square formula is A=square of a;
+    }
+}
