360027d3a1230c011d39643247c16c41a5259374
SaranyaKarthik1 <111961870+SaranyaKarthik1@users.noreply.github.com>
2022-08-24 22:25:52 +0530

    Add files via upload

---
+++ Quotient.java
@@ -0,0 +1,9 @@
+public class Quotient{
+    public static void main(String[] args) {
+        int dividend= 26, divisor = 6;
+        int quotient = dividend / divisor;
+        int remainder = dividend %divisor;
+        System.out.println("Quotient is: " + quotient);
+        System.out.println("Remainder is: " + remainder);
+    }
+}
