fa0031eff7fbf38bf2db76b34d766055fad6e219
SaranyaKarthik1 <111961870+SaranyaKarthik1@users.noreply.github.com>
2022-08-24 22:37:23 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,20 @@
+public class Positive  
+{  
+public static void main(String[] args)   
+{  
+int num=-92;  
+if(num>0)  
+{  
+System.out.println("The number is positive.");  
+}  
+else if(num<0)  
+{  
+System.out.println("The number is negative.");  
+}  
+
+else  
+{  
+System.out.println("The number is zero.");  
+}  
+}  
+}  
