e79248db6ac954eca3ff2a2ce59cb63aa381a7f1
SaranyaKarthik1 <111961870+SaranyaKarthik1@users.noreply.github.com>
2022-08-26 19:16:01 +0530

    Add files via upload

---
+++ pattern126.java
@@ -0,0 +1,18 @@
+//import java.util.*;
+public class pattern1{
+    public static void main(String[] args){
+        int n=1;
+        for(int i=1;i<=4;i++)
+        {
+             for(int j=4;j>=i;j--){
+                 System.out.print(" ");
+             }
+             for(int k=1;k<=i;k++,n++)
+              System.out.print(n+" ");
+               System.out.println( );
+             
+        }
+        
+        
+    }
+}
