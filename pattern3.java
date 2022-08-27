897d26f8087ddc561e971f3921477bb0b91620ce
SaranyaKarthik1 <111961870+SaranyaKarthik1@users.noreply.github.com>
2022-08-27 16:17:22 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class pattern3{
+    public static void main(String[] args)
+    {
+        
+        Scanner sc=new Scanner(System.in);
+         System.out.println(" enter the number ");
+         int n=sc.nextInt();char c='*';
+         for(int i=1;i<=n;i++)
+         {
+             System.out.println(c+" " );
+         }
+         
+         
+    }
+}
