29be37697cc9b9fb754bf7b6d1ebb615783deff7
SaranyaKarthik1 <111961870+SaranyaKarthik1@users.noreply.github.com>
2022-08-27 16:13:20 +0530

    Add files via upload

---
+++ Areaofconcentriccircle27.java
@@ -0,0 +1,12 @@
+ public class Areaofconcentriccircle27{
+ public static void main(String[] args){
+     double r1=5,r2=2;
+     System.out.println(" The radius of one is "+r1);
+      System.out.println(" The radius of two is "+r2);
+     double r3=((r1*r1)-(r2*r2));
+     double a=Math.PI*r3;//Assingining the value of pi=22/7
+     System.out.println(" The Area of two concentric circle is "+a);
+        
+       }
+    }
+
---
+++ Keywordlong.java
@@ -0,0 +1,8 @@
+ public class Keywordlong{
+ public static void main(String[] args){
+      long b=-56l;
+     System.out.println("the long "+b);
+        
+       }
+    }
+/*The Java long keyword is a primitive data type. It is used to declare variables. It can also be used with methods. It can hold a 64-bit two's complement integer.*/
